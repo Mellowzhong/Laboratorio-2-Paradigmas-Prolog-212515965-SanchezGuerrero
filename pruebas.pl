920e5a41608b4571653f5408c040174ddcd22abd
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2023-06-05 19:14:40 -0400

    Se agrega la funcion mkdir

--- pruebas.pl
+++ pruebas.pl
@@ -1,11 +1,20 @@
 filesystem(Name, Drives, User, Path, Rb, [Name, Drives, User, Path, Rb]).
 create_drive(Letter, Name, Capacity, Content, [Letter, Name, Capacity, Content]).
 create_user(Name, [Name]).
+set_directory(Directory, Date, [Directory, Date, [], []]).
 
+get_content_drive([_, _, _, Content], Content).
+get_current_date_time(Date) :-
+    get_time(Timestamp),
+    stamp_date_time(Timestamp, DateTime, local),
+    date_time_value(date, DateTime, Date).
 get_drives(System, Drives) :-
     filesystem(_ , Drives, _, _, _, System).
 get_user(System, Users) :-
     filesystem(_, _, Users, _, _, System).
+get_path(System, Path) :-
+    filesystem(_, _, _, Path, _, _, System).
+get_origin_path([Letter | _], Letter).
 
 username_exist(NewUser, Users) :-
     member(NewUser, [Users]).
@@ -21,6 +30,12 @@ login_exist([true | _]).
 
 login_exist([_ | Rest]) :-
     login_exist(Rest).   
+
+add_content(Drive, UpdateContent, UpdateDrive) :-
+    create_drive(Letter, Name, Capacity, _, Drive),
+    create_drive(Letter, Name, Capacity, UpdateContent, UpdateDrive).
+
+add_directory(Content, Directory, [Directory | Content]).
 
 add_drive_path(Letter, [Letter]).
 
@@ -99,3 +114,43 @@ systemSwitchDrive(System, Letter, UpdateSystem) :-
     drive_exist(Letter, Drives),
     add_drive_path(Letter, UpdateDrivePath),
     set_path(System, UpdateDrivePath, UpdateSystem).
+
+systemMkdir(System, Directory, UpdateSystem) :-
+    get_drives(System, Drives),
+    get_path(System, Path),
+    length_list(Path, Length),
+    length_path(Length),
+    set_drive(System, Drives, UpdateSystem).
+
+systemMkdir(System, Directory, UpdateSystem) :-
+    get_drives(System, Drives),
+    get_path(System, Path),
+    update_drives(Drives, Path, Directory, UpdateDrives),
+    set_drive(System, UpdateDrives, UpdateSystem).
+
+update_drives(Drives, Path, Directory, UpdateDrives):-
+    update_drives_aux(Drives, Path, Directory, [], UpdateDrives).
+
+update_drives_aux([], _, _, Acum, Acum).
+
+update_drives_aux([Drive | Rest], Path, Directory, Acum, UpdateDrives) :-
+    get_origin_path(Path, OriginPath),
+    equal(Drive, OriginPath),
+    get_content_drive(Drive, ContentDrive),
+    add_directory(ContentDrive, Directory, UpdateContent),
+    add_content(Drive, UpdateContent, UpdateDrive),
+    add_drives_to_drives(UpdateDrive, Rest, UpdateDrives).
+
+update_drives_aux([Drive | Rest], Path, Directory, Acum, UpdateDrives) :-
+    update_drives_aux(Rest, Path, Directory, [Drive | Acum], UpdateDrives).
+
+length_path(Length) :-
+    Length < 1.
+
+length_list([], 0).
+
+length_list([_ | Rest], Length) :- 
+    length(Rest, LengthTail), 
+    Length is LengthTail + 1.
+
+equal(Element, Element).
